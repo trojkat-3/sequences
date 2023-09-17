1771bc3816208c790210ef704ddc6e448702f3eb
trojkat <michal.stukow@ug.edu.pl>
2023-09-17 12:40:46 +0200

    Pre output to file

--- src/fancynumbers/FancyNumbers.java
+++ src/fancynumbers/FancyNumbers.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package fancynumbers;
 
 /**
@@ -9,13 +5,40 @@ package fancynumbers;
  * @author xenon
  */
 public class FancyNumbers {
+    
+    static final int MAX=50;
+    static final int NUM_IN_LINE=18;
+    static final int MAX_LENGTH=getNumberOfDigits(MAX);
 
+    static int getNumberOfDigits(int n){
+        int nd=0;
+        while (n>0){
+            nd++;
+            n=(n-n%10)/10;
+        }
+        return nd;
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello");
+        // Some fun with numbers
+        for (int i=1;i<MAX;i++){
+            int ndi=getNumberOfDigits(i); 
+            if (ndi<MAX_LENGTH){
+                for (int j=0;j<MAX_LENGTH-ndi;j++){
+                    System.out.print(" ");
+                }
+            } 
+            System.out.print(i);
+            if (i<MAX-1){
+                System.out.print(", ");
+            }
+            if (i % NUM_IN_LINE==0){
+                System.out.print("\n");
+            }
+        }
+        System.out.print("\n");
     }
-    
 }
