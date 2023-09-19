caa8ea1e22fdb77f011324a3f57c2a8a996c758c
trojkat <michal.stukow@ug.edu.pl>
2023-09-19 12:35:27 +0200

    Added Primes and Fibonacci.

--- src/fancynumbers/Main.java
+++ src/fancynumbers/Main.java
@@ -12,6 +12,29 @@ public class Main {
     static final int MAX = 1000;
     //static final int MAX_LENGTH = getNumberOfDigits(MAX);
 
+    static boolean isPrime(int n) {
+        if (n<2) {
+            return false;
+        } 
+        for (int i=2;i*i<=n;i++){
+            if (n%i==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    static int fibonacci(int n, ArrayList<Integer> list){
+        if (n<=1) {
+            return n;
+        } 
+        if (list.size()>n){
+            return list.get(n);
+        }    
+        return fibonacci(n-1,list)+fibonacci(n-2,list);
+        
+    }
+        
     /**
      * @param args the command line arguments
      */
@@ -30,5 +53,22 @@ public class Main {
             list.add(i * i);
         }
         printerFile.printIntegers(list);
+        //Primes
+        list.clear();
+        for (int i = 1; i <= MAX; i++) {
+            if (isPrime(i)) {
+                list.add(i);   
+            }
+        }
+        printerStdOut.printIntegers(list);
+        //Fibonacci
+        list.clear();
+        int fib;
+        int i=0;
+        while((fib=fibonacci(i,list))<=MAX){
+            list.add(fib);
+            i++;
+        }
+        printerStdOut.printIntegers(list);
     }
 }
