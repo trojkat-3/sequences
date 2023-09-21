bb8a2294a16104587d0ade776befcee63faac67e
trojkat <michal.stukow@ug.edu.pl>
2023-09-21 21:42:54 +0200

    Removed the obsolete code int the Main class.

--- src/sequences/Main.java
+++ src/sequences/Main.java
@@ -38,51 +38,5 @@ public class Main {
                 new MainFrame().setVisible(true);
             }
         });
-        
-        Printer printerFile=new PrinterFile("output/sequences.txt");
-        Printer printerStdOut=new PrinterStdOut();
-        
-        //Integers
-        Sequence sq=new Integers();
-        printerFile.print(sq);
-        //printerStdOut.print(sq);
-        //Squares
-        sq=new Squares();
-        printerFile.print(sq);
-        //printerStdOut.print(sq);
-        //Primes
-        sq=new Primes();
-        //printerFile.print(sq);
-        printerStdOut.print(sq);
-        try {
-        ArrayList decomposition=sq.decompose(32);
-        printerStdOut.PrintDecomposition(decomposition);
-        } catch(CantDecomposeException ex){
-            System.out.println("We are alvie, but:\n"+ex.getMessage());
-        }
-        
-        //Fibonacci
-        sq=new Fibonacci();
-        printerFile.print(sq);
-        printerStdOut.print(sq);
-        //Fibonacci 2
-        /*sq=new Fibonacci(1,3);
-        sq.setMax(3000);
-        printerFile.print(sq);
-        printerStdOut.print(sq);
-        
-        try {
-        System.out.println("Sum: "+ sq.getSum(30));
-        } catch(OutOfBoundsException ex) {
-            System.out.println("The program is runnig ok, however:\n "+ex.getMessage());
-        }
-        
-        sq=new Arithmetic(-7, 3);
-        printerStdOut.print(sq);
-        try {
-        System.out.println("Suma: "+sq.getSum(10));
-        } catch(OutOfBoundsException ex) {
-            System.out.println("The program is runnig ok, however:\n "+ex.getMessage());
-        }*/
     }
 }
